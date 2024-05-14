0773b292ed73ed7007e5b2fb129be015f116b519
ssutsen <babe920604@gmail.com>
2024-05-14 18:04:08 +0800

    Feat: 設定標注&側邊選單

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddProject from "./pages/AddProject";
 import TaskList from "./pages/TaskList";
 import "./App.css";
